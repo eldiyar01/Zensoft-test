6a9aaed3107d18e27d4e91922a1ba0a65a9cf4c6
eko <energeticbinge@gmail.com>
2019-05-28 02:50:36 +0600

    all done, all work

---
+++ search.py
@@ -0,0 +1,39 @@
+import difflib
+from collections import Counter
+
+
+inp = open("input.txt","r")
+pat = open("patterns.txt")
+
+print(' 1)exactly match\n 2)contain matches\n 3)contain matches with the edit\n ')
+choose = input(' Please enter number of mode:')
+print('\n------------------')
+
+patterns = pat.read().lower().splitlines()
+inputs = inp.read().lower().splitlines()
+
+
+if choose == '1':
+	for pattern in patterns:
+		search = difflib.get_close_matches(pattern, inputs, n=len(patterns), cutoff=1)
+		if search != []:
+			print(search[0])
+	print('------------------\n')
+
+
+elif choose == '2':
+	for source in inputs:
+		for destination in patterns:
+			destinationWords = destination.split()	
+			if all(word.lower() in source.lower() for word in destinationWords):
+				print(source)
+	print('------------------\n')
+
+
+elif choose == '3':
+	for pattern in patterns:
+		search = difflib.get_close_matches(pattern, inputs, n=len(patterns), cutoff=0.9)
+		if search != []:
+			print(search[0])
+	print('------------------\n')
+
